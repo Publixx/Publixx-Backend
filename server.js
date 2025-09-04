3ea3954759a83b6643235a6bec575e3c198ded8e
hitesh bhasin <hiteshbhasin@hiteshs-MacBook-Air.local>
2025-09-04 12:37:40 +0530

    server file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./src/app");
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`✅ Server running on port ${port}`));
+app.listen(port, () => console.log(`✅ Server running on portsss ${port}`));
