3e33a0fa7379b735ed067e77f288c1421cd95dfb
Harshpreet <harsh.htcoder@gmail.com>
2025-09-05 12:26:38 +0530

    email

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,13 +4,14 @@ const transporter = require("../config/mailer");
 
 async function sendOtpEmail(to, otp) {
   try {
-    await transporter.sendMail({
-    from: process.env.EMAIL_FROM || `"Publixx" <${process.env.EMAIL_USER}>`,
-    to,
-    subject: "Verify your email - Publixx",
-    text: `Your OTP is ${otp}. It will expire in 15 minutes.`,
-    html: `<p>Your OTP is <b>${otp}</b>. It will expire in 15 minutes.</p>`,
-  });
+    const info = await transporter.sendMail({
+      from: `"Publixx" <${process.env.EMAIL_FROM}>`, // ✅ Verified sender
+      to, // recipient
+      subject: "Your OTP Code",
+      text: `Your OTP is ${otp}`,
+      html: `<p>Your OTP is <b>${otp}</b></p>`,
+    })
+    console.log("Message sent: %s", info.messageId);
   } catch (error) {
     console.log("Error sending OTP email:", error);
   }  
