3f25994618dad9b825ce0e1893dbdbf86fccdc61
pawanthcoders <pawans.thcoders@gmail.com>
2025-09-08 21:36:05 +0530

    Update profile.controller.js

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -4,9 +4,7 @@ class ProfileController {
   async createOrUpdateProfile(req, res) {
     try {
       const profile = await profileService.createOrUpdateProfile(req.user, req.body, req.file);
-      console.log("=====================req.file",req);
-  // console.log("req.body",req);
-        
+         
       res.status(201).json(profile);
     } catch (err) {
       res.status(400).json({ error: err.message });
