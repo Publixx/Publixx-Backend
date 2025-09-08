c47604c5d8c68b8d1fe3f2f23bb37424d232e25c
Harshpreet <harsh.htcoder@gmail.com>
2025-09-08 18:49:31 +0530

    logs

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -4,6 +4,8 @@ class ProfileController {
   async createOrUpdateProfile(req, res) {
     try {
       const profile = await profileService.createOrUpdateProfile(req.user, req.body, req.file);
+      console.log("req.file",req.file);
+      
       res.status(201).json(profile);
     } catch (err) {
       res.status(400).json({ error: err.message });
